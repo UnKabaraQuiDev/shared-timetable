863eaec2cdf492850958cc26f732c85ed4c7a20d
UnKabaraQuiDev <66266372+UnKabaraQuiDev@users.noreply.github.com>
2025-08-14 18:13:28 +0200

    UserNotifierService working

--- shared-timetable-server/src/main/java/lu/kbra/shared_timetable/server/services/UserNotifierService.java
+++ shared-timetable-server/src/main/java/lu/kbra/shared_timetable/server/services/UserNotifierService.java
@@ -1,14 +1,27 @@
 package lu.kbra.shared_timetable.server.services;
 
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lu.kbra.shared_timetable.server.db.datas.TimetableEventData;
+import lu.kbra.shared_timetable.server.ws.UserNotifierWS;
 
 @Service
 public class UserNotifierService {
 
+	private static final Logger LOGGER = Logger.getLogger(UserNotifierService.class.getName());
+
+	@Autowired
+	private UserNotifierWS userNotifierWS;
+
 	public void notifyEventCreated(TimetableEventData event) {
-		
+		final Object payload = event;
+
+		final int count = userNotifierWS.getWebSocketHandler().broadcast("/new-event", payload);
+
+		LOGGER.info("Notified " + count + " about new event: " + event.toString());
 	}
 
 }
